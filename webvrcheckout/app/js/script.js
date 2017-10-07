cb1672e41e4a504788231caafea5248b37a41748
manokhinv <vm0339380@gmail.com>
2017-10-07 19:18:58 -0400

    New stuff

--- webvrcheckout/app/js/script.js
+++ webvrcheckout/app/js/script.js
@@ -1,47 +1,21 @@
-AFRAME.registerComponent('interactable', {
+AFRAME.registerComponent('product', {
     schema: {
-        on: {default: 'click'},
-        clickAction: {type: 'string'},
-        hoverAction: {type: 'string'},
-        keyCode: {type: 'number', default: null},
-    },
+        hoverColor: { default: '#fff'},
+        rating: { default: '' },
+        latestReview: { default: '' }
+    },    
     init: function () {
-        var _this = this;
-        var data = this.data;
-        var el = this.el;
-
-        if(data.keyCode){
-            window.addEventListener("keydown", function (event) {
-                if(event.keyCode == data.keyCode){                  
-                    console.log("key press by interactable : " + data.keyCode);
-                    el.emit('click');
-                }
-                event.preventDefault();
-            }, true);
-        }
-
-        el.addEventListener(data.on, function (evt) {            
-            var clickActionFunctionName = _this.clickAction;
-            console.log("in button, clickActionFunctionName: "+clickActionFunctionName);
-            // find object
-            var clickActionFunction = window[clickActionFunctionName];
-            //console.log("clickActionFunction: "+clickActionFunction);
-            // is object a function?
-            if (typeof clickActionFunction === "function") clickActionFunction();
+        var _this = this;  
+        var el = this.el;  
+        
+        el.addEventListener('mouseenter', function () {
+            el.setAttribute('color', _this.data.hoverColor);  
         });
-
-    },
-    update: function () {
-    },
-    tick: function () {
-    },
-    remove: function () {
-    },
-    pause: function () {
-    },
-    play: function () {
-    },
-    setClickAction: function (action) {
-        this.data.clickAction = action; //change function dynamically
-    },
+        el.addEventListener('mouseleave', function () {
+            el.setAttribute('color', '#fff');  
+        });
+        el.addEventListener('click', function () {
+            console.log("clicked " + el);
+        });
+    } 
 });
