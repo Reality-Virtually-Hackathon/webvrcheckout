b8a17f5eac8e4fb7270c3380a357fb16e361b10a
rdub80 <rdub80@gmail.com>
2017-10-07 22:31:51 -0400

    integrated aframe gui component

--- app/js/script.js
+++ app/js/script.js
@@ -1,21 +1,62 @@
+
+
 AFRAME.registerComponent('product', {
     schema: {
-        hoverColor: { default: '#fff'},
-        rating: { default: '' },
-        latestReview: { default: '' }
-    },    
-    init: function () {
-        var _this = this;  
-        var el = this.el;  
-        
+        on: {default: 'click'},
+        toggle: {type: 'boolean', default: false},
+        latestReview: {type: 'string', default: ''},
+        rating: {type: 'string', default: ''}
+    },
+    init: function() {
+
+        var data = this.data;
+        var el = this.el;
+        var guiItem = el.getAttribute("gui-item");
+        console.log("in button, guiItem: "+JSON.stringify(guiItem));
+        var guiInteractable = el.getAttribute("gui-interactable");
+        console.log("in button, guiInteractable: "+JSON.stringify(guiInteractable));
+        var toggleState = this.toggleState = data.toggle;
+
+        console.log("in gui-button init, data: "+JSON.stringify(data));
+
         el.addEventListener('mouseenter', function () {
-            el.setAttribute('color', _this.data.hoverColor);  
+            el.setAttribute('material', 'color', '#333');
         });
+
         el.addEventListener('mouseleave', function () {
-            el.setAttribute('color', '#fff');  
+            if (!(data.toggle)) {
+                el.setAttribute('material', 'color', "#fff");
+            }
         });
-        el.addEventListener('click', function () {
-            console.log("clicked " + el);
+
+        el.addEventListener(data.on, function (evt) {            
+            if (!(data.toggle)) { // if not toggling flashing active state
+            }else{
+            }
+            var clickActionFunctionName = guiInteractable.clickAction;
+            console.log("in button, clickActionFunctionName: "+clickActionFunctionName);
+            // find object
+            var clickActionFunction = window[clickActionFunctionName];
+            //console.log("clickActionFunction: "+clickActionFunction);
+            // is object a function?
+            if (typeof clickActionFunction === "function") clickActionFunction();
         });
-    } 
+
+
+    },
+    play: function () {
+
+    },
+    update: function (oldData) {
+        console.log("In button update, toggle: "+this.toggleState);
+    },
+    setActiveState: function (activeState) {
+        console.log("in setActiveState function");
+        this.data.toggle = this.toggleState = activeState;
+        if (!activeState) {
+            console.log('state toggle off');
+        } else {
+            console.log('state toggle on');
+        }
+    }
 });
